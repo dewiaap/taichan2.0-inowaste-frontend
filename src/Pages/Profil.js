5d81412b192b1c5af4f7f13467e8a3e0eba6fcad
auryhs <arumabrori7@gmail.com>
2022-05-20 01:03:36 +0700

    profile

--- src/Pages/Profil.js
+++ src/Pages/Profil.js
@@ -1,6 +1,18 @@
 const Profil = () => {
     return (
         <>
+        <div className="grid grid-cols-3 items mt-16 mb-14 mx-28">
+            <div className="box-border bg-white w-72 h-72 drop-shadow-md rounded-md">
+                <div className="box-border bg-white border-b h-10"><p className="font-extrabold ml-8 mt-6 text-lg">Hi, Arum</p></div>
+                <ul className="m-8 text-sm">
+                    <li>Akun Saya</li>
+                    <li>Poin Saya</li>
+                    <li>Riwayat Transaksi</li>
+                    <li>Log Out</li>
+                </ul>
+            </div>
+            <div className="container col-start-2 col-span-2 box-border bg-white w-110 h-auto border border-2 rounded-md justify-between pb-6"></div>
+        </div>
         </>
     );
 }
