3dea0bb9516c3044111702ca57a412839bc82bbe
unknown <dewiayupurnama333@gmail.com>
2022-05-25 02:38:44 +0700

    fixed responsive navbar, berita, home, mitra

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,107 @@
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <>
-            <div className="flex justify-between items-center h-20 w-full bg-primary px-12 font-poppins text-white sticky top-0">
-                <div className=""><a href="#/"><img src="inowaste2.png" className="object-cover h-20"></img></a></div>
-                <div className="">
-                    <span className="ml-6 font-bold text-xl hover:text-tertiary"><a href="#/#beranda">Beranda</a></span>
-                    <span className="ml-6 font-bold text-xl hover:text-tertiary"><a href="#/mitra">Daftar Mitra</a></span>
-                    <span className="ml-6 font-bold text-xl hover:text-tertiary"><a href="#/berita">Berita</a></span>
+        <div className="sticky top-0 z-50">
+            <nav className="bg-tertiary md:bg-primary">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex justify-between items-center h-20 w-full font-poppins text-white">
+                        <div className=""><a href="#/"><img src="inowaste2.png" className="object-cover h-20"></img></a></div>
+                        <div className="hidden md:flex">
+                            <span className="ml-6 font-bold text-xl hover:text-tertiary"><a href="#/#beranda">Beranda</a></span>
+                            <span className="ml-6 font-bold text-xl hover:text-tertiary"><a href="#/mitra">Daftar Mitra</a></span>
+                            <span className="ml-6 font-bold text-xl hover:text-tertiary"><a href="#/berita">Berita</a></span>
+                        </div>
+                        <div className="hidden md:flex">
+                            <button className="bg-secondary hover:bg-tertiary px-8 py-2 rounded-full font-bold text-xl"><a href="#/masuk">Masuk</a></button>
+                        </div>
+                        <div className="-mr-2 flex md:hidden">
+                            <button
+                                onClick={() => setIsOpen(!isOpen)}
+                                type="button"
+                                className="bg-tertiary inline-flex items-center justify-center p-2 text-white focus:outline-none"
+                                aria-controls="mobile-menu"
+                                aria-expanded="false"
+                            >
+                                <span className="sr-only"></span>
+                                {!isOpen ? (
+                                    <svg
+                                        className="block h-8 w-8"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        className="block h-8 w-8"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className="">
-                    <button className="bg-secondary hover:bg-tertiary px-8 py-2 rounded-full font-bold text-xl"><a href="#/masuk">Masuk</a></button>
-                </div>
-            </div>
-        </>
+
+                <Transition
+                    show={isOpen}
+                    enter="transition ease-out duration-100 transform"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="transition ease-in duration-75 transform"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
+                >
+                    {() => (
+                        <div className="md:hidden" id="mobile-menu">
+                            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                                <a
+                                    href="#/#beranda"
+                                    className="text-white hover:text-secondary block px-3 py-2 rounded-md text-xl font-medium"
+                                >
+                                    Beranda
+                                </a>
+
+                                <a
+                                    href="#/mitra"
+                                    className="text-white hover:text-secondary block px-3 py-2 rounded-md text-xl font-medium"
+                                >
+                                    Daftar Mitra
+                                </a>
+
+                                <a
+                                    href="#/berita"
+                                    className="text-white hover:text-secondary block px-3 py-2 rounded-md text-xl font-medium"
+                                >
+                                    Berita
+                                </a>
+                            </div>
+                        </div>
+                    )}
+                </Transition>
+            </nav>
+        </div>
     );
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,98 +1,109 @@
 const Home = () => {
     return (
         <>
-        <div className="grid grid-cols-2 w-full h-fit bg-primary ">
-            <div className="font-poppins ml-16">
-                <h1 className="font-bold text-5xl text-white mt-52">Be Hero to Zero Waste</h1>
-                <p className="font-medium text-2xl text-white mt-11"><strong>inowaste</strong> merupakan platform berbasis website yang dapat 
-                    mempertemukan pengepul dengan penghasil minyak jelantah dengan mudah.</p>
-                <button className=" rounded-full px-12 py-2 w-56 mt-8 text-center p-3 bg-secondary text-white font-bold hover:bg-tertiary mt-6 text-lg focus:outline-none my-1">Tukar Minyak
-                </button>
+            <div className="grid grid-cols-1 md:grid-cols-2 h-fit bg-primary pt-12 md:pt-12">
+                <div className="flex flex-col self-center font-poppins pl-12 pr-12 md:pr-0 order-last md:order-first">
+                    <h1 className="font-bold text-5xl text-white">Be Hero to Zero Waste</h1>
+                    <p className="font-medium text-xl md:text-2xl text-white mt-8 text-justify md:text-left"><strong>inowaste</strong> merupakan platform berbasis website yang dapat
+                        mempertemukan pengepul dengan penghasil minyak jelantah dengan mudah.</p>
+                    <div className="flex justify-center md:justify-start">
+                        <button className="rounded-full px-12 py-2 w-full md:w-56 mt-8 text-center p-3 bg-secondary text-white font-bold hover:bg-tertiary text-lg focus:outline-none my-1">Tukar Minyak
+                        </button>
+                    </div>
+                </div>
+                <div className="flex justify-center mb-6 md:mb-0">
+                    <img src="home_img.png" className="w-9/12"></img>
+                </div>
             </div>
-            <div className="mt-28 ml-8">
-                <img src="home_img.png"></img>
+            <div className="">
+                <img src="vector_wave.png"></img>
             </div>
-        </div>
-        <div>
-            <img src="vector_wave.png"></img>
-        </div>
-        <div className="font-bold font-poppins text-primary text-5xl mt-16 mb-8 text-center">
-            <h1>Langkah Penukaran Minyak</h1>
-        </div>
-        <div className="flex justify-center mb-28">
-            <img src="line_home.png"></img>
-        </div>
-        <div className="w-5/6 mx-24 mb-32">
-            <img src="langkah_img.png"></img>
-        </div>
-        <div className="grid grid-cols-2 border border-2 font-poppins text-white h-72 drop-shadow-xl mb-20">
-            <div className=" bg-primary py-10 px-10">
-                <h1 className="font-bold text-3xl">VISI KAMI</h1>
-                <p className="mt-8 text-xl">Menjaga kelestarian lingkungan dengan cara membantu menyalurkan minyak jelantah</p>
+            <div className="grid grid-cols-1">
+                <div className="font-bold font-poppins text-primary text-3xl md:text-5xl mt-16 mb-8 text-center px-12 md:px-0">
+                    <h1>Langkah Penukaran Minyak</h1>
+                </div>
+                <div className="flex justify-center mb-28">
+                    <img src="line_home.png"></img>
+                </div>
+                <div className="flex w-5/6 justify-center mx-auto mb-32">
+                    <img src="langkah_img.png"></img>
+                </div>
             </div>
-            <div className=" bg-secondary py-10 px-10 text-right">
-                <h1 className="font-bold text-3xl">MISI KAMI</h1>
-                <p className="mt-8 text-xl">1. Mempertemukan pengepul dengan penghasil minyak jelantah <br></br>
-                    2. Mengajak dan mengedukasi masyarakat menyalurkan minyak jelantah</p>
+            <div className="grid grid-cols-2 font-poppins text-white">
+                <div className=" bg-primary py-10 px-10">
+                    <h1 className="font-bold text-3xl">VISI KAMI</h1>
+                    <p className="mt-8 text-md md:text-xl">Menjaga kelestarian lingkungan dengan cara membantu menyalurkan minyak jelantah</p>
+                </div>
+                <div className=" bg-secondary py-10 px-10 text-right">
+                    <h1 className="font-bold text-3xl">MISI KAMI</h1>
+                    <p className="mt-8 text-md md:text-xl">1. Mempertemukan pengepul dengan penghasil minyak jelantah <br></br>
+                        2. Mengajak dan mengedukasi masyarakat menyalurkan minyak jelantah</p>
+                </div>
             </div>
-        </div>
-        <div className="font-bold font-poppins text-primary text-5xl mt-16 mb-8 text-center">
-            <h1>Daftar Mitra</h1>
-        </div>
-        <div className="flex justify-center mb-20">
-            <img src="line_home.png"></img>
-        </div>
-        <div className="px-11 mb-10">
-            <div className="flex space-x-14 place-content-center mt-8">
-                    <a href="" className="container flex box-border border w-48 h-64 p-2  shadow-lg rounded-md grid grid-cols-1 devide-y">
+            <div className="grid grid-cols-1">
+                <div className="font-bold font-poppins text-primary text-3xl md:text-5xl mt-16 mb-8 text-center">
+                    <h1>Daftar Mitra</h1>
+                </div>
+                <div className="flex justify-center mb-6 md:mb-20">
+                    <img src="line_home.png"></img>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-4 md:gap-8 mx-auto">
+                    <a href="" className="container box-border border w-48 h-64 p-2 shadow-lg rounded-md mb-6 md:mb-0">
                         <div className="box-content w-16 h-4 py-1 rounded-lg bg-secondary text-xs text-center font-bold text-primary font-poppins justify-self-end">
-                        <p>309 poin</p>
+                            <p>309 poin</p>
                         </div>
-                        <img src="logoGEU.png" alt="pict" className="px-4 mt-4 object-cover h-28 w-44"></img>
+                        <div className="flex justify-center">
+                            <img src="logoGEU.png" alt="pict" className="object-contain h-28 w-44"></img>
+                        </div>
                         <div>
                             <p className="font-halant text-sm font-bold mt-2">PT Green Energi Utama (GEU)</p>
                             <p className="pt-4 font-poppins text-xs text-gray-500 pb-2 text-right">| Malang</p>
                         </div>
                     </a>
 
-                    <a href="" className="container flex box-border border w-48 h-64 p-2  shadow-lg rounded-md grid grid-cols-1 devide-y">
+                    <a href="" className="container box-border border w-48 h-64 p-2 shadow-lg rounded-md mb-6 md:mb-0">
                         <div className="box-content w-16 h-4 py-1 rounded-lg bg-secondary text-xs text-center font-bold text-primary font-poppins justify-self-end">
-                        <p>309 poin</p>
+                            <p>309 poin</p>
                         </div>
-                        <img src="logoGEU.png" alt="pict" className="px-4 mt-4 object-cover h-28 w-44"></img>
+                        <div className="flex justify-center">
+                            <img src="logoGEU.png" alt="pict" className="object-contain h-28 w-44"></img>
+                        </div>
                         <div>
                             <p className="font-halant text-sm font-bold mt-2">PT Green Energi Utama (GEU)</p>
                             <p className="pt-4 font-poppins text-xs text-gray-500 pb-2 text-right">| Malang</p>
                         </div>
                     </a>
 
-                    <a href="" className="container flex box-border border w-48 h-64 p-2  shadow-lg rounded-md grid grid-cols-1 devide-y">
+                    <a href="" className="container box-border border w-48 h-64 p-2 shadow-lg rounded-md mb-6 md:mb-0">
                         <div className="box-content w-16 h-4 py-1 rounded-lg bg-secondary text-xs text-center font-bold text-primary font-poppins justify-self-end">
-                        <p>309 poin</p>
+                            <p>309 poin</p>
                         </div>
-                        <img src="logoGEU.png" alt="pict" className="px-4 mt-4 object-cover h-28 w-44"></img>
+                        <div className="flex justify-center">
+                            <img src="logoGEU.png" alt="pict" className="object-contain h-28 w-44"></img>
+                        </div>
                         <div>
                             <p className="font-halant text-sm font-bold mt-2">PT Green Energi Utama (GEU)</p>
                             <p className="pt-4 font-poppins text-xs text-gray-500 pb-2 text-right">| Malang</p>
                         </div>
                     </a>
 
-                    <a href="" className="container flex box-border border w-48 h-64 p-2  shadow-lg rounded-md grid grid-cols-1 devide-y">
+                    <a href="" className="container box-border border w-48 h-64 p-2 shadow-lg rounded-md mb-6 md:mb-0">
                         <div className="box-content w-16 h-4 py-1 rounded-lg bg-secondary text-xs text-center font-bold text-primary font-poppins justify-self-end">
-                        <p>309 poin</p>
+                            <p>309 poin</p>
                         </div>
-                        <img src="logoGEU.png" alt="pict" className="px-4 mt-4 object-cover h-28 w-44"></img>
+                        <div className="flex justify-center">
+                            <img src="logoGEU.png" alt="pict" className="object-contain h-28 w-44"></img>
+                        </div>
                         <div>
                             <p className="font-halant text-sm font-bold mt-2">PT Green Energi Utama (GEU)</p>
                             <p className="pt-4 font-poppins text-xs text-gray-500 pb-2 text-right">| Malang</p>
                         </div>
                     </a>
+                </div>
+                <div className="flex justify-center">
+                    <button className="rounded-full w-full md:w-3/6 text-center p-3 bg-secondary text-white hover:bg-tertiary text-xl font-bold focus:outline-none my-16"><a href="#/mitra">Lihat Semua</a></button>
+                </div>
             </div>
-        </div>
-        <div>
-        <button className=" rounded-full w-3/6 mx-72 text-center p-3 bg-secondary text-white hover:bg-tertiary text-xl font-bold focus:outline-none my-16"><a href="#/mitra">Lihat Semua</a></button>
-        </div>
-
         </>
     );
 }
