1678b63e771d8fcb19475ce86ad4d88dcf2677dd
unknown <dewiayupurnama333@gmail.com>
2022-05-26 18:43:59 +0700

    change riwayat, poin

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
                 )
                 :
                 (
-                    <div className="sticky top-0 z-50">
+                    <div className="sticky top-0 z-10">
                         <nav className="bg-tertiary md:bg-primary">
                             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                                 <div className="flex justify-between items-center h-20 w-full font-poppins text-white">
